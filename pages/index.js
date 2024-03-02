8975b025318611274174bc9b7a2ce20fd7dba3cd
BlueKoopa0127 <bluekoopa@me.com>
2024-03-03 04:46:14 +0900

    最初のコミット

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import { Sample } from '../components/sample';
+import { useRecoilState } from 'recoil';
+import { atom } from 'recoil';
+
+export const countState = atom({
+  key: 'countState',
+  default: 0,
+});
+
+export default function Home({ Title }) {
+  const [count, setCount] = useRecoilState(countState);
+  return (
+    <div>
+      <div>{Title}</div>
+      <div>選択するとこ</div>
+      <div>結果の表示</div>
+      <div>ちゃーと</div>
+      <Sample />
+      <div
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        {count}
+      </div>
+    </div>
+  );
+}
+export async function getStaticProps() {
+  const t = 'たいとる';
+  return { props: { Title: t } };
+}
